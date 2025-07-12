2f4c06857519d032b2b2c694078b0ae2b464d111
Firmansyahp16 <firman.jka@gmail.com>
2025-07-12 10:49:02 +0700

    Rmv: Nest API

--- api-nest/src/utils/generateID.ts
+++
@@ -1,7 +0,0 @@
-import { customAlphabet } from 'nanoid';
-
-export function generateID(startWith: string) {
-  const numberPart = customAlphabet('0123456789', 3)();
-  const letterPart = customAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 3)();
-  return `${startWith}-${numberPart}-${letterPart}`;
-}
